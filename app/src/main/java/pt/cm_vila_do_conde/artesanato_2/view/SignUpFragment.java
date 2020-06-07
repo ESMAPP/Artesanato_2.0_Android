9b57a9ac3c4b721e19e4b9e1b17d576c694c66d7
solecity <solecity@hotmail.com>
2020-06-07 12:27:17 +0100

    sign up validations

--- app/src/main/java/pt/cm_vila_do_conde/artesanato_2/view/SignUpFragment.java
+++ app/src/main/java/pt/cm_vila_do_conde/artesanato_2/view/SignUpFragment.java
@@ -2,65 +2,91 @@ package pt.cm_vila_do_conde.artesanato_2.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import pt.cm_vila_do_conde.artesanato_2.R;
+import pt.cm_vila_do_conde.artesanato_2.databinding.FragmentSignupBinding;
+import pt.cm_vila_do_conde.artesanato_2.viewmodel.AuthViewModel;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignUpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SignUpFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SignUpFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignUpFragment newInstance(String param1, String param2) {
-        SignUpFragment fragment = new SignUpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentSignupBinding binding;
+    private AuthViewModel AuthViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+
+        binding = FragmentSignupBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initAuthViewModel();
+        initBtnSubmit();
+    }
+
+    private void initAuthViewModel() {
+        AuthViewModel = new ViewModelProvider(requireActivity()).get(AuthViewModel.class);
+    }
+
+    public void initBtnSubmit() {
+        binding.btnSubmit.setOnClickListener(v -> signUp());
+    }
+
+    private void signUp() {
+        String name = binding.inputName.getText().toString();
+        String email = binding.inputEmail.getText().toString();
+        String password = binding.inputPassword.getText().toString();
+        String confirmPassword = binding.inputConfirmPassword.getText().toString();
+
+        runValidations(name, email, password, confirmPassword);
+
+
+        // chamar authviewmodel
+        // send to database
+
+    }
+
+    private void runValidations(String name, String email, String password, String confirmPassword) {
+        String errMsg = "Este campo tem de estar preenchido";
+
+        if (name.isEmpty()) {
+            binding.inputName.setError(errMsg);
+        }
+
+        if (email.isEmpty()) {
+            binding.inputEmail.setError(errMsg);
+        }
+
+        if (password.isEmpty()) {
+            binding.inputPassword.setError(errMsg);
+        }
+
+        if (confirmPassword.isEmpty()) {
+            binding.inputConfirmPassword.setError(errMsg);
+        }
+
+        if (password != confirmPassword) {
+            errMsg = "As passwords não são iguais";
+
+            binding.inputConfirmPassword.setError(errMsg);
+        }
     }
 }

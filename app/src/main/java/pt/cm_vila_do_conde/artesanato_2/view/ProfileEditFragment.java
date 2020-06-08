68f7e6c473ada8805e956f39754a7ce4a8b2c393
solecity <solecity@hotmail.com>
2020-06-08 01:51:50 +0100

    home menu navigation

--- app/src/main/java/pt/cm_vila_do_conde/artesanato_2/view/ProfileEditFragment.java
+++ app/src/main/java/pt/cm_vila_do_conde/artesanato_2/view/ProfileEditFragment.java
@@ -1,34 +1,41 @@
 package pt.cm_vila_do_conde.artesanato_2.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import org.jetbrains.annotations.NotNull;
 
 import pt.cm_vila_do_conde.artesanato_2.R;
-import pt.cm_vila_do_conde.artesanato_2.databinding.FragmentHomeBinding;
 import pt.cm_vila_do_conde.artesanato_2.databinding.FragmentProfileEditBinding;
 
 public class ProfileEditFragment extends Fragment {
     FragmentProfileEditBinding binding;
 
     @Override
-    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileEditBinding.inflate(inflater, container, false);
-        View view = binding.getRoot();
-        return view;
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.btnBack.setOnClickListener(v -> gotBack());
+    }
+
+    private void gotBack() {
+        Navigation.findNavController(requireView()).navigate(R.id.action_profileEditFragment_to_profileFragment);
     }
 }

7d58c79c446fb7de1177dee1cb416518e9ede108
ImInYourPie <iminyourpie@gmail.com>
2020-06-10 23:32:50 +0100

    User role fetch in home

---
+++ app/src/main/java/pt/cm_vila_do_conde/artesanato_2/repository/HomeRepository.java
@@ -0,0 +1,37 @@
+package pt.cm_vila_do_conde.artesanato_2.repository;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import pt.cm_vila_do_conde.artesanato_2.model.User;
+
+public class HomeRepository {
+    private String TAG = "HOME_REPOSITORY";
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+    private CollectionReference usersRef = rootRef.collection("users");
+
+    public MutableLiveData<Integer> getUserRole(){
+        MutableLiveData<Integer> userType = new MutableLiveData<>();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null){
+            usersRef.document(firebaseUser.getUid()).get().addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    User user = document.toObject(User.class);
+                    userType.setValue(user.getType());
+                } else {
+                    Log.e(TAG, task.getException().getLocalizedMessage());
+                }
+            });
+        }
+        return userType;
+    }
+}
--- app/src/main/java/pt/cm_vila_do_conde/artesanato_2/viewmodel/HomeViewModel.java
+++ app/src/main/java/pt/cm_vila_do_conde/artesanato_2/viewmodel/HomeViewModel.java
@@ -1,4 +1,27 @@
 package pt.cm_vila_do_conde.artesanato_2.viewmodel;
 
-public class HomeViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import pt.cm_vila_do_conde.artesanato_2.repository.HomeRepository;
+
+public class HomeViewModel extends AndroidViewModel {
+    public LiveData<Integer> userRole;
+    private HomeRepository homeRepository;
+
+    public HomeViewModel(@NonNull Application application) {
+        super(application);
+        homeRepository = new HomeRepository();
+    }
+
+    /*public void addUserFromNavigation(User user){
+        userMutableLiveData.setValue(user);
+    }*/
+
+    public void getUserRole(){
+        userRole = homeRepository.getUserRole();
+    }
 }

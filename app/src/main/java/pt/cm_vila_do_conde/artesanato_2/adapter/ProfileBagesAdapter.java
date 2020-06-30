e0b1189170dfc691629a4a405cbd311acb0965d5
fspereiraDev <filipe-pereira95@hotmail.com>
2020-06-30 19:42:46 +0100

    profile load info profile load badges(not completed)

---
+++ app/src/main/java/pt/cm_vila_do_conde/artesanato_2/adapter/ProfileBagesAdapter.java
@@ -0,0 +1,28 @@
+package pt.cm_vila_do_conde.artesanato_2.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import pt.cm_vila_do_conde.artesanato_2.adapter.viewholder.BagdesViewHolder;
+
+public class ProfileBagesAdapter extends RecyclerView.Adapter<BagdesViewHolder>  {
+
+
+    @NonNull
+    @Override
+    public BagdesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BagdesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/pt/cm_vila_do_conde/artesanato_2/adapter/viewholder/BagdesViewHolder.java
@@ -0,0 +1,22 @@
+package pt.cm_vila_do_conde.artesanato_2.adapter.viewholder;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import pt.cm_vila_do_conde.artesanato_2.R;
+
+public class BagdesViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView image;
+
+    public BagdesViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+
+
+
+    }
+}

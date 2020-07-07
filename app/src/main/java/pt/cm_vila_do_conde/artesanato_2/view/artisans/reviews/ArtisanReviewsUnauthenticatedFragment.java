ba066c3fc3ab4fa4ac3c6aaebf6c03a7ee4882a9
solecity <solecity@hotmail.com>
2020-07-07 22:17:16 +0100

    add cardview event list add placeholder image to home cards add placeholder image to unauthenticated reviews fragment

--- app/src/main/java/pt/cm_vila_do_conde/artesanato_2/view/artisans/reviews/ArtisanReviewsUnauthenticatedFragment.java
+++ app/src/main/java/pt/cm_vila_do_conde/artesanato_2/view/artisans/reviews/ArtisanReviewsUnauthenticatedFragment.java
@@ -5,11 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.jetbrains.annotations.NotNull;
+
 import pt.cm_vila_do_conde.artesanato_2.R;
+import pt.cm_vila_do_conde.artesanato_2.databinding.FragmentViewPagerEmptyBinding;
+
 
 public class ArtisanReviewsUnauthenticatedFragment extends Fragment {
+    FragmentViewPagerEmptyBinding binding;
 
     public ArtisanReviewsUnauthenticatedFragment() {
         // Required empty public constructor
@@ -21,8 +28,26 @@ public class ArtisanReviewsUnauthenticatedFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_artisan_reviews_unauthenticated, container, false);
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentViewPagerEmptyBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setEmptyState();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setEmptyState();
+    }
+
+    private void setEmptyState() {
+        requireActivity().findViewById(R.id.artisan_page).setBackgroundResource(R.color.white);
+        binding.textEmptyState.setText(getText(R.string.label_need_login_reviews));
+        binding.imageEmptyState.setImageResource(R.drawable.ic_placeholder_key_color);
     }
 }
